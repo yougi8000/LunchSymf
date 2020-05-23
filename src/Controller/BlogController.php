8014acae627c175bc1cc79d9503aee5cd9d6a85b
Mike <yougi8000@gmail.com>
2020-05-23 19:32:03 +0200

    for anthony

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,21 +6,36 @@ use App\Entity\Article;
 use App\Entity\ArticleCategory;
 use App\Repository\ArticleRepository;
 use App\Repository\ArticleCategoryRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface; // Nous appelons le bundle KNP Paginator
+
+
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
-    {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
 
-        $articles = $repo->findAll();
 
-        //$articles->setCategory($category);   ManyToOne je ne sais pas si ça marche
+        public function index(Request $request, PaginatorInterface $paginator) // Nous ajoutons les paramètres requis
+        {
+        //$repo = $this->getDoctrine()->getRepository(Article::class);
+
+        //$articles = $repo->findAll();
+
+            // Méthode findBy qui permet de récupérer les données avec des critères de filtre et de tri
+            $donnees = $this->getDoctrine()->getRepository(Article::class)->findBy([],['createdAt' => 'desc']);
+    
+            $articles = $paginator->paginate(
+                $donnees, // Requête contenant les données à paginer (ici nos articles)
+                $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+                4 // Nombre de résultats par page
+            );
+            
+            //$articles->setCategory($category);   ManyToOne je ne sais pas si ça marche
 
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
@@ -28,6 +43,7 @@ class BlogController extends AbstractController
         ]);
     }
 
+        
     /**
      * @Route("/blog/home", name="homeblog")
      */
